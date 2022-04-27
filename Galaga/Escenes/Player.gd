a48177f9c123c3dd1be4957ae995de041d39c3ba
OVyla <101257154+OVyla@users.noreply.github.com>
2022-04-27 13:27:11 +0200

    Programat el moviment en la nau especial

--- Galaga/Escenes/Player.gd
+++ Galaga/Escenes/Player.gd
@@ -1,14 +1,28 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var velocitat = 200
+var direccio = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	pass
+	direccio = Vector2.ZERO
+	
+	if Input.is_action_pressed("Mov_dreta"):
+		direccio += Vector2.RIGHT
+	if Input.is_action_pressed("Mov_esq"):
+		direccio += Vector2.LEFT
+		
+	position += direccio.normalized() * velocitat * delta
+	
+
+
+func _on_Player_area_entered(area):
+	$AnimatedSprite.play("Detonació")
+
+
+func _on_AnimatedSprite_animation_finished():
+	$AnimatedSprite.stop()
+	$AnimatedSprite.set_visible(false)
