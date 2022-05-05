98ea100a64662fcce6726437bf0e15150e468573
PSR234 <91489042+PSR234@users.noreply.github.com>
2022-05-05 08:58:57 +0200

    Ànec
    l'ànec ja està a punt de ser completament funcional

--- Duck Hunt/Personatges/Duck.gd
+++ Duck Hunt/Personatges/Duck.gd
@@ -2,26 +2,43 @@ extends KinematicBody2D
 
 var vel = Vector2()
 var speed = 300
-var dir := Vector2.DOWN
+var dir := Vector2.RIGHT
+var angle = 0
 
 func _ready():
 	randomize()
-	dir = dir.rotated(rand_range(0,2*PI))
+	angle = rand_range((-1.0/3)*PI,(1.0/3)*PI)*((randi() % 2) * 2 - 1)
+	dir = Vector2.RIGHT
+	dir = dir.rotated(angle)
 	$moviment.wait_time = rand_range(1, 2)
 	$moviment.start()
 
 func _process(delta):
 	if is_on_wall():
-		dir.x *-1
+		dir.x *= -1
 	if is_on_ceiling() or is_on_floor():
-		dir.y *-1
-		
+		dir.y *= -1
 	
-	move_and_slide(dir * speed)
+	
+	
+	vel = move_and_slide(dir * speed, Vector2.UP)
+	anima(vel)
 
+func anima(vel):
+	if vel.x > 0:
+		$AnimatedSprite.flip_h = false
+	elif vel.x < 0:
+		$AnimatedSprite.flip_h = true
+	
+	if rad2deg(angle) > 30 or rad2deg(angle) < -30:
+		$AnimatedSprite.play("Vol amunt")
+	elif rad2deg(angle) <= 30 and rad2deg(angle) >= -30:
+		$AnimatedSprite.play("Vol horitzontal")
 
 func change_dir():
-	dir = dir.rotated(rand_range(0,2*PI))
-	$moviment.wait_time = rand_range(0.4, 2)
+	angle = rand_range((-1.0/3)*PI,(1.0/3)*PI)*((randi() % 2) * 2 - 1)
+	dir = Vector2.RIGHT
+	dir = dir.rotated(angle)
+	$moviment.wait_time = rand_range(1, 2)
 func _on_moviment_timeout():
 	change_dir()
