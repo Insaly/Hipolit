b87e96a5216cad3097a9ba85bc15d659c4064459
PSR234 <91489042+PSR234@users.noreply.github.com>
2022-05-11 13:28:00 +0200

    Progrés
    Ànecs ja quasi acabats, inicis del hud ja fets (falta tota la part de programa

--- Duck Hunt/Personatges/Duck.gd
+++ Duck Hunt/Personatges/Duck.gd
@@ -1,26 +1,42 @@
 extends KinematicBody2D
 
 var vel = Vector2()
-var speed = 300
+var speed = 0
 var dir := Vector2.RIGHT
 var angle = 0
+var mouse_in = true
+var cacat = false
+var caient = false
+
 
 func _ready():
 	randomize()
-	angle = rand_range((-1.0/3)*PI,(1.0/3)*PI)*((randi() % 2) * 2 - 1)
+	speed = rand_range(200, 300)
+	angle = rand_range((-1.0/3)*PI,(1.0/3)*PI) + ((randi() % 2) * 180)
 	dir = Vector2.RIGHT
 	dir = dir.rotated(angle)
 	$moviment.wait_time = rand_range(1, 2)
 	$moviment.start()
 
 func _process(delta):
-	if is_on_wall():
-		dir.x *= -1
-	if is_on_ceiling() or is_on_floor():
-		dir.y *= -1
-	
-	
-	
+	if cacat:
+		dir = Vector2(0,0)
+		$AnimatedSprite.play("Impacte")
+		$Mort.start()
+		
+	elif caient:
+		dir = Vector2.DOWN
+		$AnimatedSprite.play("Mort")
+		$Mort2.start()
+	else:
+		if is_on_wall():
+			dir.x *= -1
+		if is_on_ceiling() or is_on_floor():
+			dir.y *= -1
+		
+		if Input.is_action_just_pressed("Clic") and mouse_in == true:
+			cacat = true
+		
 	vel = move_and_slide(dir * speed, Vector2.UP)
 	anima(vel)
 
@@ -30,15 +46,36 @@ func anima(vel):
 	elif vel.x < 0:
 		$AnimatedSprite.flip_h = true
 	
-	if rad2deg(angle) > 30 or rad2deg(angle) < -30:
+	if rad2deg(angle) > 30 or rad2deg(angle) < -30 and (abs(vel.x) > 0 or abs(vel.y) > 0):
 		$AnimatedSprite.play("Vol amunt")
-	elif rad2deg(angle) <= 30 and rad2deg(angle) >= -30:
+	elif rad2deg(angle) <= 30 and rad2deg(angle) >= -30 and (abs(vel.x) > 0 or abs(vel.y) > 0):
 		$AnimatedSprite.play("Vol horitzontal")
 
 func change_dir():
-	angle = rand_range((-1.0/3)*PI,(1.0/3)*PI)*((randi() % 2) * 2 - 1)
+	angle = rand_range((-1.0/3)*PI,(1.0/3)*PI) + ((randi() % 2) * 180)
 	dir = Vector2.RIGHT
 	dir = dir.rotated(angle)
 	$moviment.wait_time = rand_range(1, 2)
+
 func _on_moviment_timeout():
 	change_dir()
+
+
+
+
+
+func _on_Duck_mouse_entered():
+	mouse_in = true
+
+func _on_Duck_mouse_exited():
+	mouse_in = false
+
+
+
+func _on_Mort_timeout():
+	cacat = false
+	caient = true
+
+
+func _on_Mort2_timeout():
+	queue_free()
