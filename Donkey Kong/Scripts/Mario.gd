7d691de64525c9bbdecee6ff89f7a3b62cb0395b
Insaly <81804810+Insaly@users.noreply.github.com>
2022-05-18 11:42:39 +0200

    Donkey Kong v.0.2

--- Donkey Kong/Scripts/Mario.gd
+++ Donkey Kong/Scripts/Mario.gd
@@ -6,7 +6,10 @@ var velocitat = Vector2.ZERO
 
 var direccio = Vector2.DOWN
 var gravetat = Vector2.DOWN * 980
-var velocitat_salt = -400
+var velocitat_salt = -270
+
+var escales = false
+var animacio = false
 
 func _process(delta):
 	pass
@@ -18,17 +21,37 @@ func _physics_process(delta):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("Mov_esq"):
 		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_just_pressed("Mov_amunt") and is_on_floor():
+	if Input.is_action_just_pressed("Tecla_x") and is_on_floor():
 		velocitat.y = velocitat_salt
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 	anima(velocitat)
 	
+	if escales and Input.is_action_pressed("Mov_amunt"):
+		gravetat = Vector2.ZERO
+		velocitat.y = -30
+		$Animacions.play("escalant")
+	elif not animacio:
+		gravetat = Vector2.DOWN * 980
+		
 func anima(velocitat):
-	if velocitat.x == 0:
-		$Animacions.play("normal")
-	if velocitat.x > 0:
-		$Animacions.play("caminant")
-		$Animacions.flip_h = false
-	if velocitat.x < 0:
-		$Animacions.play("caminant")
-		$Animacions.flip_h = true
+	if animacio == false:
+		if velocitat.x == 0 and velocitat.y == 0:
+			$Animacions.play("normal")
+		if velocitat.x > 0:
+			$Animacions.flip_h = false
+			if is_on_floor():
+				$Animacions.play("caminant")
+		if velocitat.x < 0:
+			$Animacions.flip_h = true
+			if is_on_floor():
+				$Animacions.play("caminant")
+		if Input.is_action_just_pressed("Tecla_x"):
+			$Animacions.play("salt")
+
+func _on_Area2D_body_entered(body):
+	if body is KinematicBody2D:
+		escales = true
+		
+func _on_Area2D_body_exited(body):
+	if body is KinematicBody2D:
+		escales = false
